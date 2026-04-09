477d83d7fd86be3f4cc9175c9a285b823503fbc5
Fisher48 <rybakov48@yandex.ru>
2026-04-09 07:45:15 +0300

    Created BoardState

---
+++ src/main/java/ru/fisher/BoardState.java
@@ -0,0 +1,11 @@
+package ru.fisher;
+
+public class BoardState {
+    Board board;
+    int score;
+
+    public BoardState(Board board, int score) {
+        this.board = board;
+        this.score = score;
+    }
+}
