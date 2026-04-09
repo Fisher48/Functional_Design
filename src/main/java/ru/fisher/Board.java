6d3053ffc0cb22b579f32afd5c634728a17df6f9
Fisher48 <rybakov48@yandex.ru>
2026-04-09 07:44:58 +0300

    Created Board

---
+++ src/main/java/ru/fisher/Board.java
@@ -0,0 +1,16 @@
+package ru.fisher;
+
+public class Board {
+    int size;
+    Element[][] cells;
+
+    public Board(int s) {
+        this.size = s;
+        cells = new Element[size][size];
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                cells[x][y] = new Element(Element.EMPTY);
+            }
+        }
+    }
+}
